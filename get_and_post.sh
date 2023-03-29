b2eeaae4fbf2c0c7d8a8c1d6447e928a963ae174
Patrick Mineault <patrick.mineault@gmail.com>
2023-03-29 12:00:23 -0400

    Fix path

--- get_and_post.sh
+++ get_and_post.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Run the Python script
-conda run -n gpt python get_toots.py
+PATH=$PATH:/home/pmin/.cache/selenium/chromedriver/linux64/111.0.5563.64/
+/home/pmin/miniconda3/bin/conda run -n gpt python get_toots.py
+/home/pmin/miniconda3/bin/conda run -n gpt python post_toots.py
